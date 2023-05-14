2455414ebb3fd45a7279e66183ab42e8c88b8b02
gonzales <gonzalesitokawa@gmail.com>
2023-05-14 12:23:21 +0900

    add: Add Service Method

--- src/src/service/ArticleService/ArticleService.ts
+++ src/src/service/ArticleService/ArticleService.ts
@@ -1,17 +1,26 @@
 import connectToDatabase from "@/lib/mongoose/connect/connect";
+import ArticleModel from "@/lib/mongoose/models/ArticleModel";
+import IArticle from "@/types/Article/IArticle";
 
-class ArticleService {
+export default class ArticleService {
   async connectToDatabase() {
     await connectToDatabase();
   }
 
-  static async getAllArticles() {
-
+  static async getArticlesLimitSeven() {
+    const articles: IArticle[] = JSON.parse(
+      JSON.stringify(await ArticleModel.find({}).limit(7))
+    );
+    return articles;
   }
 
+  static async getAllArticles() {
+    const articles: IArticle = JSON.parse(
+      JSON.stringify(await ArticleModel.find({}))
+    );
+    return articles;
+  }
 }
 
 const Article = new ArticleService();
 Article.connectToDatabase();
-
-export default Article;
--- src/src/service/CategoryService/CategoryService.ts
+++ src/src/service/CategoryService/CategoryService.ts
@@ -1,17 +1,34 @@
 import connectToDatabase from "@/lib/mongoose/connect/connect";
+import CategoryModel from "@/lib/mongoose/models/CategoryModel";
 
-class CategoryService {
+export default class CategoryService {
   async connectToDatabase() {
     await connectToDatabase();
   }
 
-  static async getAllArticles() {
-
+  static async getAllCategories() {
+    const categories = JSON.parse(JSON.stringify(await CategoryModel.find({})));
+    return categories;
   }
 
+  static async getCategoriesWithoutSelf(categoryID: string) {
+    const categories = JSON.parse(
+      JSON.stringify(await CategoryModel.find({ _id: { $ne: categoryID } }))
+    );
+    return categories;
+  }
+
+  static async getAllArticlesBelongToCategory(categoryID: string) {
+    const articles = JSON.parse(
+      JSON.stringify(
+        await CategoryModel.findOne({ _id: categoryID })
+          .populate("articles")
+          .exec()
+      )
+    );
+    return articles;
+  }
 }
 
 const Category = new CategoryService();
 Category.connectToDatabase();
-
-export default Category;
